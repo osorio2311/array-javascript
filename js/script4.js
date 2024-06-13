2eb4eb3005d59c43ee3e28b14160013fbb92bc9f
Jose Luis Osorio <osorio231173@hotmail.com>
2024-06-13 16:23:56 +0200

    #

--- js/script4.js
+++ js/script4.js
@@ -16,15 +16,21 @@
 // }
 
 
+/**
+ Crea una aplicación de análisis de datos en la que estás trabajando necesitas crear una estructura que almacena 10 múltiplos de 5 aleatorios de 0 a 100. Además, ni el primer elemento puede ser menor de 50, ni el último mayor de 50.
+ */
+
+ //crea Array 10 elementos
 let datos = new Array(10);
 
+//crea los 10 elementos random
 for (let i = 0; i < 10; i++) {
     datos[i] = Math.floor(Math.random() * 20 + 1) * 5;
   }
 original = datos.slice();
 console.log(original);
 
-
+//si el primero es menor de 50 lo cambia
 if(datos[0]<50){
     datos.shift();
     let num;
@@ -35,6 +41,7 @@ if(datos[0]<50){
     console.log("cambio el primero:",datos)
 }
 
+//si el último es mayor de 50 lo sustituye
 if(datos[0]>50){
     datos.pop();
     let numero;
